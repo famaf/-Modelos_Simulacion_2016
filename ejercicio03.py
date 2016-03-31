313c084a605f26ceb2be8a675fc6701286fdf8eb
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-03-30 23:22:49 -0300

    Ejercicio 3d terminado y Ejercicio 4a terminado

--- ejercicio03.py
+++ ejercicio03.py
@@ -59,16 +59,16 @@ def ejercicio03c(n):
 
     return (a/float(n))
 
-# ESTE NO FUNCIONA BIEN
+
 def ejercicio03d(n):
     """
     Ejercicio 3d del Practico 3.
     """
     a = 0
-    for i in xrange(1, n+1):
+    for i in xrange(n):
         w = random.random()
         z = random.random()
-        a += math.e**(((w**i) + (z**i))**2)
+        a += math.e**((w + z)**2)
 
     return (a/float(n))
 
@@ -76,18 +76,18 @@ def ejercicio03d(n):
 L = [100, 1000, 10000, 100000, 1000000]
 
 print "-------------------------------------------------------------"
-# for n in L:
-#     #print "n = {0} => Ejercicio 3a = {1}" .format(n, ejercicio03a(n))
-#     print "n = %d => Ejercicio 3a = %f" % (n, ejercicio03a(n))
-# print "-------------------------------------------------------------"
+for n in L:
+    #print "n = {0} => Ejercicio 3a = {1}" .format(n, ejercicio03a(n))
+    print "n = %d => Ejercicio 3a = %f" % (n, ejercicio03a(n))
+print "-------------------------------------------------------------"
 
-# for n in L:
-#     print "n = {0} => Ejercicio 3b = {1}" .format(n, ejercicio03b(n))
-# print "-------------------------------------------------------------"
+for n in L:
+    print "n = {0} => Ejercicio 3b = {1}" .format(n, ejercicio03b(n))
+print "-------------------------------------------------------------"
 
-# for n in L:
-#     print "n = {0} => Ejercicio 3c = {1}" .format(n, ejercicio03c(n))
-# print "-------------------------------------------------------------"
+for n in L:
+    print "n = {0} => Ejercicio 3c = {1}" .format(n, ejercicio03c(n))
+print "-------------------------------------------------------------"
 
 for n in L:
     print "n = {0} => Ejercicio 3d = {1}" .format(n, ejercicio03d(n))
--- ejercicio04.py
+++ ejercicio04.py
@@ -5,30 +5,26 @@ import math
 
 
 def ejercicio04a(n):
-    a = 0
-    last_factorial = 1
-    for i in xrange(1, n+1):
-        a += 1.0/(last_factorial*i)
-        last_factorial = last_factorial*i
+    """
+    Ejercicio 4a del Practico 3.
+    """
+    a = 0 # Acumulador de la suma de n tq' min{ n : Sn > 1 }
+    for i in xrange(n):
+        N = 2 # Es el n tq' min{ n : Sn > 1 } el min n que lo cumple es 2
+        s = random.random() + random.random()
 
-    return a+1
+        # Si s es menor a 1 => sumamos un nuevo numero aleatorio y
+        # actualizamos el n tq' min{ n : Sn > 1 }
+        while s <= 1.0:
+            s += random.random()
+            N += 1
+
+        a += N
+
+    return float(a)/n
 
 
 L = [100, 1000, 10000, 100000, 1000000]
 
-print ejercicio04a(170) # si n > 170 da error: overflow
-
-#print math.e
-# for n in L:
-#     print "e =", ejercicio04a(n)
-
-
-
-# def factorial(n):
-#     last_factorial = 1
-#     for i in range(1, n+1):
-#         last_factorial *= i
-
-#     return last_factorial
-
-# print factorial(6)
+for n in L:
+    print "e =", ejercicio04a(n)
