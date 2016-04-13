935c1da381ba724d5e68d5991aed084f156cefe9
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-04-13 18:01:33 -0300

    Ejercicio 4 y 5 terminados

--- Practico_04/ejercicio04.py
+++ Practico_04/ejercicio04.py
@@ -32,38 +32,13 @@ def ejercicio04():
     return x
 
 
-print ejercicio04()
+def esperanza(n):
+    a = 0
+    for _ in xrange(n):
+        a += ejercicio04()
+
+    return a/float(n)
 
 
-# def ejercicio04(n):
-#     """
-#     Ejericicio 04 del Practico 4.
-#     """
-#     a = 0
-#     for _ in xrange(n):
-#         # Metodo de composicion
-#         u = random.random()
-#         if u < 0.5:
-#             # Metodo de la Transformada Inversa
-#             u = random.random()
-#             j = 1
-#             pj = 0.5**j
-#             while u >= pj:
-#                 j += 1
-#                 pj += 0.5**j
-#             x = j
-#         else:
-#             # Metodo de la Transformada Inversa
-#             u = random.random()
-#             j = 1
-#             pj = 0.5*((float(2)/3)**j)
-#             while u >= pj:
-#                 j += 1
-#                 pj += 0.5*((float(2)/3)**j)
-#             x = j
-#         a += x
-
-#     return float(a)/n
-
-# for n in [100, 1000, 10000, 100000]:
-#     print "n =", n, "--> E(X) =", ejercicio04(n)
+for n in [100, 1000, 10000, 100000]:
+    print "n =", n, "--> E(X) =", esperanza(n)
--- Practico_04/ejercicio05.py
+++ Practico_04/ejercicio05.py
@@ -4,34 +4,78 @@ import random
 import math
 
 
-def poisson(n, lamb):
-    pn = math.exp(-lamb)
-    i = 1
-    while i <= n:
-        pn *= (lamb/float(i))
-        i += 1
-   
-    return pn
-
-
-def ejercicio05(k, l):
-    pn = 1
-    j = 1
-    while j <= k:
-        pn += (l/float(i))
-        j += 1
-    
-    denominador = math.exp(-l)*pn
-    
+def poisson(lamda):
+    # Te devuelve una X con distribución Poisson con parámetro lamda
     u = random.random()
-    u *= denominador
-    
     i = 0
-    
-    pi = 1
-    
-    while u >= math.exp(-l)*pi:
-        i += 1
-        pi += 
+    p = math.exp(-lamda)
+    f = p
 
-print ejercicio05(2, 1)
+    while u >= f:
+        p = (lamda*p)/float(i+1)
+        f += p
+        i += 1
+
+    x = i
+
+    return x
+
+
+def ejercicio05_inversa(k, lamda):
+
+    pj = 0 # Acumula los sumandos del denomindor
+    j = 0 
+    while j <= k:
+        pj += (lamda**j/float(math.factorial(j)))
+        j += 1
+
+    divisor = pj # Divisor obtenido de la sumatoria
+
+    # Algoritmo de generacion de una va Poisson P(lamda)
+    u = random.random()
+    i = 0
+    pi = 1/float(divisor) # Primer elemento de la productoria
+    f = pi
+
+    while u >= f:
+        pi = (lamda*pi)/float(i+1)
+        f += pi
+        i += 1
+
+    x = i
+
+    return x
+
+
+def ejercicio05_rechazo(k, lamda):
+    y = poisson(lamda)
+    while y >= k:
+        y = poisson(lamda)
+    return y
+
+
+def esperanza_1(k, lamda, n):
+    a = 0
+    for _ in xrange(n):
+        a += ejercicio05_inversa(k, lamda)
+
+    return a/float(n)
+
+
+def esperanza_2(k, lamda, n):
+    a = 0
+    for _ in xrange(n):
+        a += ejercicio05_rechazo(k, lamda)
+
+    return a/float(n)
+
+
+print "Metodo de Transformada Inversa"
+for n in [100, 1000, 10000, 100000]:
+    print "n =", n, "--> E(X) =", esperanza_1(5, 2, n)
+
+print "------------------------------"
+
+print "Metodo de Aceptacion y Rechazo"
+for n in [100, 1000, 10000, 100000]:
+    print "n =", n, "--> E(X) =", esperanza_2(5, 2, n)
