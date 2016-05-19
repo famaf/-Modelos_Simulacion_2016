78226e290938340eef4d8e08dda50e796827eb24
Mario E. Ferreyra <marioezequiel.ferreyra@yahoo.com.ar>
2016-05-19 12:52:39 -0300

    Directorio para el practico 7 y comienzo del ejercicio 1

---
+++ Practico_07/ejercicio01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import math
+import random
+
+p_flor_blanca = 1/4.0
+p_flor_rosa = 1/2.0
+p_flor_roja = 1/4.0
+
+GUISANTES = 564
+
+flores_blancas = 141
+flores_rosas = 291
+flores_rojas = 132
+
