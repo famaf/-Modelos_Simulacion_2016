65e7393c1579eb79c361ce075be6b3e320a097cf
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-05-24 18:13:25 -0300

    Datos de los ejericicios 9 y 10

---
+++ Practico_07/ejercicio09.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import math
+import random
+from distribuciones import *
+
+
+def simulacion():
+    tratamiento1 = [65.2, 67.1, 69.4, 78.4, 74, 80.3]
+    tratamiento2 = [59.4, 72.1, 68, 66.2, 58.5]
---
+++ Practico_07/ejercicio10.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import math
+import random
+from distribuciones import *
+
+
+def simulacion():
+    tratamiento = [19, 31, 39, 45, 47, 66, 75]
+    control = [28, 36, 44, 49, 52, 72, 72]
