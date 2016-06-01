505618fb7e8a939dc1679537ac1800a1af572ffd
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-06-01 00:05:58 -0300

    Avances en los ejercicios 9 y 10

--- Practico_07/ejercicio09.py
+++ Practico_07/ejercicio09.py
@@ -6,6 +6,9 @@ from distribuciones import *
 
 
 def sumaRangos(muestra1, muestra2):
+    """
+    Calcula la suma de los rangos (R) de la muestra 1.
+    """
     muestra = muestra1 + muestra2
     muestra.sort()
 
@@ -23,28 +26,37 @@ def P(n, m, k):
     """
     Calcula la probabilidad P (dado n, m) de k.
     """
+    # Casos base 1: n = 1 y m = 0
     if n == 1 and m == 0:
         if k <= 0:
             result = 0
         else:
             result = 1
+    # Casos base 2: n = 0 y m = 1
     elif n == 0 and m == 1:
         if k < 0:
             result = 0
         else:
             result = 1
+    # Casos Recursivos: n >= 1 y m >= 1
     else:
+        # Caso especial 1: n = 0 y m >= 2
         if n == 0:
             result = (m/float(m+n)) * P(n, m-1, k)
+        # Caso especial 2: n >= 2 y m >= 0
         elif m == 0:
             result = (n/float(n+m)) * P(n-1, m, k-n-m)
+        # Caso general
         else:
             result = (n/float(n+m)) * P(n-1, m, k-n-m) + (m/float(m+n)) * P(n, m-1, k)
 
     return result
 
 
-def simulacion():
+def valorExacto():
+    """
+    Calculo exacto del p-valor.
+    """
     muestra1 = [65.2, 67.1, 69.4, 78.4, 74, 80.3]
     muestra2 = [59.4, 72.1, 68, 66.2, 58.5]
 
@@ -56,28 +68,94 @@ def simulacion():
 
     R = sumaRangos(muestra1, muestra2)
 
+    # Calculo de p-valor por recursion
     p_valor = 2 * min(P(n, m, R), 1 - P(n, m, R-1))
 
     return p_valor
 
 
-def simulacion2():
+def aproximacionNormal():
+    """
+    Calculo del p-valor, con una aproximacion normal.
+    """
     muestra1 = [65.2, 67.1, 69.4, 78.4, 74, 80.3]
     muestra2 = [59.4, 72.1, 68, 66.2, 58.5]
+
+    # muestra1 = [132, 104, 162, 171, 129]
+    # muestra2 = [107, 94, 136, 99, 114, 122, 108, 130, 106, 88]
 
     n = len(muestra1) # Tamaño de la primera muestra
     m = len(muestra2) # Tamaño de la segunda muestra
 
-    N = n + m
+    N = n + m # Tamaño de la suma de las muestras
 
     R = sumaRangos(muestra1, muestra2)
 
-    numerador = R - n*((N+1)/2.0)
-    denominador = math.sqrt(n * m * ((N+1)/12.0))
+    esp_R = n * (N+1)/2.0 # Esperanza de R
+    var_R = n * m * (N+1)/12.0 # Varianza de R
 
-    w = numerador/denominador
+    z = (R - esp_R)/math.sqrt(var_R) # Normalizacion a una N(0, 1)
+
+    # Calculo del p-valor
+    if R <= esp_R:
+        p_valor = 2 * fi(z)
+    else:
+        p_valor = 2 * (1 - fi(z))
+
+    return p_valor
+
+
+def simulacion(k):
+    """
+    Calculo del p-valor, con una simulacion
+    """
+    muestra1 = [65.2, 67.1, 69.4, 78.4, 74, 80.3]
+    muestra2 = [59.4, 72.1, 68, 66.2, 58.5]
+
+    # muestra1 = [132, 104, 162, 171, 129]
+    # muestra2 = [107, 94, 136, 99, 114, 122, 108, 130, 106, 88]
+
+    n = len(muestra1) # Tamaño de la primera muestra
+    m = len(muestra2) # Tamaño de la segunda muestra
+
+    N = n + m # Tamaño de la suma de las muestras
+
+    r = sumaRangos(muestra1, muestra2)
+
+    # Lista con las dos muestras ordenadas
+    muestra = muestra1 + muestra2
+    muestra.sort()
+
+    R_mayor = 0 # Cantidad de veces que R >= r
+    R_menor = 0 # Cantidad de veces que R <= r
+
+    # Ejecutamos k veces la simulacion
+    for _ in xrange(k):
+        sub_muestra = [] # Contendra la submuestra
+        while len(sub_muestra) < n:
+            valor = muestra[random.randint(0, N-1)] # 
+            
+            # Si el valor no esta en la submuestra, lo agrego
+            if valor not in sub_muestra:
+                sub_muestra.append(valor)
+
+        R = sumaRangos(sub_muestra, muestra2) # Generamos el R correspondiente
+
+        if R >= r:
+            R_mayor += 1
+        elif R <= r:
+            R_menor += 1
+
+    pro_R_mayor = R_mayor/float(k)
+    pro_R_menor = R_menor/float(k)
+
+    # Calculo del p-valor
+    p_valor = 2 * min(pro_R_menor, pro_R_mayor)
+
+    return p_valor
 
 
 
-
-print simulacion()
+print "Valor Exacto =", valorExacto()
+print "Aprocimacion Normal =", aproximacionNormal()
+print "Simulacion =", simulacion(100000)
--- Practico_07/ejercicio10.py
+++ Practico_07/ejercicio10.py
@@ -6,6 +6,9 @@ from distribuciones import *
 
 
 def sumaRangos(muestra1, muestra2):
+    """
+    Calcula la suma de los rangos (R) de la muestra 1.
+    """
     muestra = muestra1 + muestra2
     muestra.sort()
 
@@ -19,13 +22,131 @@ def sumaRangos(muestra1, muestra2):
     return R
 
 
-def simulacion():
+def P(n, m, k):
+    """
+    Calcula la probabilidad P (dado n, m) de k.
+    """
+    # Casos base 1: n = 1 y m = 0
+    if n == 1 and m == 0:
+        if k <= 0:
+            result = 0
+        else:
+            result = 1
+    # Casos base 2: n = 0 y m = 1
+    elif n == 0 and m == 1:
+        if k < 0:
+            result = 0
+        else:
+            result = 1
+    # Casos Recursivos: n >= 1 y m >= 1
+    else:
+        # Caso especial 1: n = 0 y m >= 2
+        if n == 0:
+            result = (m/float(m+n)) * P(n, m-1, k)
+        # Caso especial 2: n >= 2 y m >= 0
+        elif m == 0:
+            result = (n/float(n+m)) * P(n-1, m, k-n-m)
+        # Caso general
+        else:
+            result = (n/float(n+m)) * P(n-1, m, k-n-m) + (m/float(m+n)) * P(n, m-1, k)
+
+    return result
+
+
+def valorExacto():
+    """
+    Calculo exacto del p-valor.
+    """
     muestra1 = [19, 31, 39, 45, 47, 66, 75]
     muestra2 = [28, 36, 44, 49, 52, 72, 72]
 
+    n = len(muestra1) # Tamaño de la primera muestra
+    m = len(muestra2) # Tamaño de la segunda muestra
+
     R = sumaRangos(muestra1, muestra2)
 
-    print R
+    # Calculo de p-valor por recursion
+    p_valor = 2 * min(P(n, m, R), 1 - P(n, m, R-1))
+
+    return p_valor
 
 
-simulacion()
+def aproximacionNormal():
+    """
+    Calculo del p-valor, con una aproximacion normal.
+    """
+    muestra1 = [19, 31, 39, 45, 47, 66, 75]
+    muestra2 = [28, 36, 44, 49, 52, 72, 72]
+
+    n = len(muestra1) # Tamaño de la primera muestra
+    m = len(muestra2) # Tamaño de la segunda muestra
+
+    N = n + m # Tamaño de la suma de las muestras
+
+    R = sumaRangos(muestra1, muestra2)
+
+    esp_R = n * (N+1)/2.0 # Esperanza de R
+    var_R = n * m * (N+1)/12.0 # Varianza de R
+
+    z = (R - esp_R)/math.sqrt(var_R) # Normalizacion a una N(0, 1)
+
+    # Calculo del p-valor
+    if R <= esp_R:
+        p_valor = 2 * fi(z)
+    else:
+        p_valor = 2 * (1 - fi(z))
+
+    return p_valor
+
+
+def simulacion(k):
+    """
+    Calculo del p-valor, con una simulacion
+    """
+    muestra1 = [19, 31, 39, 45, 47, 66, 75]
+    muestra2 = [28, 36, 44, 49, 52, 72, 72]
+
+    n = len(muestra1) # Tamaño de la primera muestra
+    m = len(muestra2) # Tamaño de la segunda muestra
+
+    N = n + m # Tamaño de la suma de las muestras
+
+    r = sumaRangos(muestra1, muestra2)
+
+    # Lista con las dos muestras ordenadas
+    muestra = muestra1 + muestra2
+    muestra.sort()
+
+    R_mayor = 0 # Cantidad de veces que R >= r
+    R_menor = 0 # Cantidad de veces que R <= r
+
+    # Ejecutamos k veces la simulacion
+    for _ in xrange(k):
+        sub_muestra = [] # Contendra la submuestra
+        while len(sub_muestra) < n:
+            valor = muestra[random.randint(0, N-1)] # 
+            
+            # Si el valor no esta en la submuestra, lo agrego
+            if valor not in sub_muestra:
+                sub_muestra.append(valor)
+
+        R = sumaRangos(sub_muestra, muestra2) # Generamos el R correspondiente
+
+        if R >= r:
+            R_mayor += 1
+        elif R <= r:
+            R_menor += 1
+
+    pro_R_mayor = R_mayor/float(k)
+    pro_R_menor = R_menor/float(k)
+
+    # Calculo del p-valor
+    p_valor = 2 * min(pro_R_menor, pro_R_mayor)
+
+    return p_valor
+
+
+
+print "Valor Exacto =", valorExacto()
+print "Aprocimacion Normal =", aproximacionNormal()
+print "Simulacion =", simulacion(100000)
