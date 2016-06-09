3613d744271cbf74776712ca6fc41691f4fa46c3
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-06-08 22:48:35 -0300

    Comienzo muy laxo del trabajo especial

---
+++ Trabajos_Especiales/TE02/trabajo.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from pagerank import *
+
+ejemplo = ((2,), (0, 2), (1,))
+# telefonica = ((0, 1), (1, 0))
+
+############################
+### Funciones Auxiliares ###
+############################
+
+# def gradoEntrada(matriz, nodo):
+#     d_pos = sum(matriz[nodo])
+
+#     return d_pos
+
+
+
+
+#############################
+### Funciones Principales ###
+#############################
+
+def caminanteAleatorio(grafo):
+    """
+    Ejercicio 1.
+    """
+    n = len(grafo) # Tamaño del grafo
+
+    # Obtenemos la Matriz de Transicion
+    P = g2p(grafo)
+
+    matriz = []
+    # for i in xrange(n):
+    #     matriz[i][0]
+
+
+def tiempoCruce():
+    """
+    Ejercicio 2.
+    """
+    pass
+
+
+def tiempoCubrimiento():
+    """
+    Ejercicio 3.
+    """
+    pass
+
+
+def ejercicio04():
+    """
+    Ejercicio 4.
+    """
+    pass
+
+
+def paginasFictias():
+    """
+    Ejercicio 5a.
+    """
+    pass
+
+
+def hackearPagina():
+    """
+    Ejercicio 5b.
+    """
+    pass
